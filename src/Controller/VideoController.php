397dd4915d6f56d142b499f6e25fba325b22da8d
Mims <amine.chr2@gmail.com>
2024-08-26 00:24:29 +0200

    first commit

---
+++ src/Controller/VideoController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Video;
+use App\Form\VideoType;
+use App\Repository\VideoRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/')]
+class VideoController extends AbstractController
+{
+    #[Route('/', name: 'app_video_index', methods: ['GET'])]
+    public function index(VideoRepository $videoRepository): Response
+    {
+        return $this->render('video/index.html.twig', [
+            'videos' => $videoRepository->findAll(),
+        ]);
+    }
+    #[Route('/api/video', methods: ['GET'])]
+    public function indexApi(EntityManagerInterface $em): Response
+    {
+        $videos = $em->getRepository(Video::class)->findAll();
+        return $this->json($videos , 200 , [] , [
+            "groups" => ['videos.index' , 'videos.show']
+        ]);
+        
+    }
+
+    #[Route('/new', name: 'app_video_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $video = new Video();
+        $form = $this->createForm(VideoType::class, $video);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $this->getUser();  // Récupère l'utilisateur connecté
+        $video->setUser($user);    // Associe l'utilisateur à la vidéo
+        
+        $video->setCreatedAt(new \DateTimeImmutable());
+        $video->setUpdatedAt(new \DateTimeImmutable());
+        
+        $em->persist($video);
+        $em->flush();
+        $this->addFlash('success', '👍 Votre vidéo a bien été créée!');
+
+            return $this->redirectToRoute('app_video_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('video/new.html.twig', [
+            'video' => $video,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_video_show', methods: ['GET'])]
+    public function show(Video $video): Response
+    {
+        return $this->render('video/show.html.twig', [
+            'video' => $video,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_video_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Video $video, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(VideoType::class, $video);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            return $this->redirectToRoute('app_video_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('video/edit.html.twig', [
+            'video' => $video,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_video_delete', methods: ['POST'])]
+    public function delete(Request $request, Video $video, EntityManagerInterface $em): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$video->getId(), $request->getPayload()->getString('_token'))) {
+            $em->remove($video);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('app_video_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/Video.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VideoRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use App\Entity\Traits\Timestampable;
+
+#[ORM\Entity(repositoryClass: VideoRepository::class)]
+#[ORM\Table(name: 'videos')]
+class Video
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['videos.index'])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    #[Groups(['videos.index'])]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 500)]
+    #[Groups(['videos.index'])]
+    private ?string $videoLink = null;
+
+    #[Groups(['videos.show'])]
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    use Timestampable;
+
+    #[ORM\ManyToOne(inversedBy: 'videos')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getVideoLink(): ?string
+    {
+        return $this->videoLink;
+    }
+
+    public function setVideoLink(string $videoLink): static
+    {
+        $this->videoLink = $videoLink;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
